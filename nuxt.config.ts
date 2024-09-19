c86abbe5fd698f97dab68846de2447ffa5c86f35
unknown <adamszuta@vp.pl>
2024-09-19 23:31:43 +0200

    feat: rework structure

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
-})
+  devtools: { enabled: process.env.NODE_ENV === 'development' },
+  runtimeConfig: {
+    baseURL: process.env.BASE_URL,
+    public: {
+      baseURL: process.env.PUBLIC_BASE_URL,
+    },
+  },
+  nitro: {
+    devProxy: {
+      '/api': {
+        target: `${process.env.API_URL}/api`,
+        changeOrigin: true,
+      },
+    },
+  },
+});
