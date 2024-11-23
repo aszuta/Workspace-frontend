ae129b636491cdef2f2d1e8deb1323bc14c572d8
unknown <adamszuta@vp.pl>
2024-11-23 22:24:28 +0100

    chore: added error page

---
+++ error.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="Error">
+        <Nav />
+        <main class="Error__error-container">
+            <p class="Error__status-code">{{ error.statusCode }}</p>
+            <h1 v-if="error.statusCode === 404" class="Error__title">Strona nie została znaleziona</h1>
+            <h1 v-else class="Error__title">Wystąpił błąd</h1>
+            <nuxt-link to="/" class="Error__button">Wróć na stronę główną</nuxt-link>
+        </main>
+    </div>
+</template>
+
+<script setup>
+defineProps({
+    error: Object,
+});
+</script>
+
+<style lang="scss">
+.Error {
+    width: 100%;
+    min-height: 100vh;
+    background-color: #f9f9fb;
+
+    &__error-container {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        text-align: center;
+        flex-direction: column;
+        height: 100vh;
+    }
+
+    &__status-code {
+        color: #a33e8c;
+        font-weight: 900;
+        font-size: 30px;
+    }
+
+    &__title {
+        font-size: 50px;
+        margin-bottom: 1rem;
+    }
+
+    &__button {
+        font-size: 18px;
+        font-weight: 500;
+        color: #a33e8c;
+
+        &:hover {
+            color: #ba49a1;
+        }
+    }
+}
+</style>
